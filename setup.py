573b2c20913bd034a30e1aa23ce0a2c62930b1a5
liber_tas <b@louis.vryheid.me>
2020-11-13 15:13:08 -0500

    Added __init__.py to operators to make PyPi setup include it in package

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hausnet_server",
-    version="0.0.1",
+    version="0.0.2",
     author="Louis Calitz",
     author_email="louis@hausnet.io",
     description="A server for the HausNet protocol",
